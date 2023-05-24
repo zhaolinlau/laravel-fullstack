6d7a906f3b2519916a3c93071efb4006ae24c5a4
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-24 19:36:31 +0800

    reverse

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
     return redirect('/login');
 });
